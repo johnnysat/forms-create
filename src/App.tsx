dcb1ae84acce42ff75c7fe5321ee4528ed7f4fc2
Johnny <johnny_s.o@hotmail.com>
2023-01-31 19:32:52 -0300

    Removendo usestate do app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { InputType } from "./components/InputType/InputType";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -12,7 +10,7 @@ function App() {
       <InputType />
       <InputType />
       <InputType />
-
+      <InputType />
     </div>
   )
 }
